dce20787d52d640273f029cef85adcf4cc5b0fd9
Robert Kranz <robert.kranz@hicknhack-software.com>
2015-02-16 14:36:08 +0100

    * added timetracker association to user

--- lib/redmine_patches/user_patch.rb
+++ lib/redmine_patches/user_patch.rb
@@ -10,6 +10,7 @@ module RedminePatches
       class_eval do
         has_many :chronos_time_logs, class_name: 'Chronos::TimeLog'
         has_many :chronos_time_bookings, :through => :chronos_time_logs, class_name: 'Chronos::TimeBooking', source: :time_bookings
+        has_one :chronos_time_tracker, class_name: 'Chronos::TimeTracker'
       end
     end
 
@@ -17,6 +18,7 @@ module RedminePatches
       super
       substitute = ::User.anonymous
       Chronos::TimeLog.update_all ['user_id = ?', substitute.id], ['user_id = ?', id]
+      Chronos::TimeTracker.update_all ['user_id = ?', substitute.id], ['user_id = ?', id]
     end
   end
 end
