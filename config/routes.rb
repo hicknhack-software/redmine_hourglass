cc02088160031521f5e502735c80ed60d7311ba9
Robert Kranz <robert.kranz@hicknhack-software.com>
2015-11-09 12:11:14 +0100

    improved asset loading for development

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ scope 'chronos' do
   root to: 'chronos_overview#index'
 end
 
-mount Chronos::Assets.instance, :at => 'plugin_assets/redmine_chronos'
+unless Rails.env.production?
+  mount Chronos::Assets.instance, :at => 'plugin_assets/redmine_chronos/stylesheets'
+  mount Chronos::Assets.instance, :at => 'plugin_assets/redmine_chronos/javascripts'
+end
--- lib/chronos/assets.rb
+++ lib/chronos/assets.rb
@@ -3,7 +3,8 @@ class Chronos::Assets < Sprockets::Environment
 
   def initialize
     super File.join(File.dirname(__FILE__), '..', '..') do |env|
-      env.append_path 'app/assets'
+      env.append_path 'app/assets/javascripts'
+      env.append_path 'app/assets/stylesheets'
       if Rails.env.production?
         env.js_compressor  = Uglifier.new
         env.css_compressor  = CSSminify.new
