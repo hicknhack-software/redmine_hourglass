03a5e23beeadc9f3b8516a7e38f53de49f763cc5
Torsten Braun <torsten.braun@hicknhack-software.com>
2015-12-09 15:17:06 +0100

    * refactored external import to use a library in the controller and model

--- app/controllers/chronos_import_controller.rb
+++ app/controllers/chronos_import_controller.rb
@@ -1,14 +1,10 @@
-require 'rake'
-
 class ChronosImportController < ApplicationController
   def redmine_time_tracker_plugin
-    load File.expand_path(File.join(File.dirname(__FILE__), '../../lib/tasks/import.rake'))
-
-    Rake::Task.define_task(:environment)
-    Rake::Task['redmine:plugins:chronos:import_redmine_time_tracker'].invoke
+    ChronosImport::RedmineTimeTracker.import!
 
     flash[:notice] = I18n::t('chronos.settings.import.time_tracker_plugin_success')
-  rescue
+  rescue => e
+    puts e
     flash[:error] = I18n::t('chronos.settings.import.time_tracker_plugin_error')
   ensure
     redirect_to plugin_settings_path(:redmine_chronos)
