07d94ba8cc7e2160657e03f1aed9388c1003e4f9
Robert Kranz <robert.kranz@hicknhack-software.com>
2015-12-30 22:14:41 +0100

    * changed the way the issue actions hook works to integrate the chronos application helper

--- lib/chronos/redmine_hooks.rb
+++ lib/chronos/redmine_hooks.rb
@@ -1,5 +1,12 @@
 class Chronos::RedmineHooks < Redmine::Hook::ViewListener
+  def view_issues_show_description_bottom(context={})
+    context[:hook_caller].content_for :header_tags, javascript_include_tag('application', plugin: 'redmine_chronos')
+    context[:hook_caller].content_for :header_tags, stylesheet_link_tag('application', plugin: 'redmine_chronos')
+    context[:hook_caller].class.send :include, Chronos::ApplicationHelper
+    context[:controller].render_to_string partial: 'hooks/issue_actions'
+  end
+
   def self.load!
-    render_on :view_issues_show_description_bottom, partial: 'hooks/issue_actions'
+    #load hook specific stuff
   end
 end
