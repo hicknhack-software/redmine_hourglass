e9dccbaec9feb302b3d14274ff3e7f03841b10be
Robert Kranz <robert.kranz@hicknhack-software.com>
2016-01-02 16:11:34 +0100

    * reorganized assets to load smaller files on no chronos ui specific pages * improved hook loading * added issue context menu hook

--- lib/chronos/redmine_hooks.rb
+++ lib/chronos/redmine_hooks.rb
@@ -1,12 +1,22 @@
 class Chronos::RedmineHooks < Redmine::Hook::ViewListener
-  def view_issues_show_description_bottom(context={})
-    context[:hook_caller].content_for :header_tags, javascript_include_tag('application', plugin: 'redmine_chronos')
-    context[:hook_caller].content_for :header_tags, stylesheet_link_tag('application', plugin: 'redmine_chronos')
-    context[:hook_caller].class.send :include, Chronos::ApplicationHelper
+  def view_issues_show_description_bottom(context = {})
+    load_assets_and_helper context[:hook_caller]
+    context[:controller].render_to_string partial: 'hooks/issue_actions'
+  end
+
+  def view_issues_context_menu_start(context = {})
+    load_assets_and_helper context[:hook_caller]
     context[:controller].render_to_string partial: 'hooks/issue_actions'
   end
 
   def self.load!
     #load hook specific stuff
+  end
+
+  private
+  def load_assets_and_helper(view_context)
+    view_context.content_for :header_tags, javascript_include_tag('global', plugin: 'redmine_chronos')
+    view_context.content_for :header_tags, stylesheet_link_tag('global', plugin: 'redmine_chronos')
+    view_context.class.send :include, Chronos::ApplicationHelper
   end
 end
