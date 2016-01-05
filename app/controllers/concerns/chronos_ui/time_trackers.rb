6d74845c637fc66404173af39c36b97e4f922f19
Robert Kranz <robert.kranz@hicknhack-software.com>
2016-01-05 12:25:37 +0100

    * added an edit form for time trackers

--- app/controllers/concerns/chronos_ui/time_trackers.rb
+++ app/controllers/concerns/chronos_ui/time_trackers.rb
@@ -11,5 +11,18 @@ module ChronosUi
       init_sort
       @list_arguments = list_arguments
     end
+
+    def edit_time_trackers
+      time_tracker = get_time_tracker
+      authorize_foreign
+      render 'chronos_ui/time_trackers/edit', locals: {time_tracker: time_tracker}, layout: false unless performed?
+    end
+
+    private
+    def get_time_tracker
+      time_tracker = Chronos::TimeTracker.find_by id: params[:id]
+      render_404 unless time_tracker.present?
+      @request_resource = time_tracker
+    end
   end
 end
