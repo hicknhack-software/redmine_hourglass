2b8bbd67931d3b3a58c7eda495f2f59b533069c7
Robert Kranz <robert.kranz@hicknhack-software.com>
2016-01-29 15:32:50 +0100

    * fixed strange fake bugs with random times in the morning or afternoon

--- spec/factories/chronos_time_bookings.rb
+++ spec/factories/chronos_time_bookings.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     transient do
       user { create :user }
     end
-    start { Faker::Time.between Time.now, Time.now, :morning }
-    stop { Faker::Time.between Time.now, Time.now, :afternoon }
+    start { Faker::Time.between Date.today, Date.today, :morning }
+    stop { Faker::Time.between Date.today, Date.today, :afternoon }
     time_log { create(:time_log, start: start, stop: stop, user: user) }
     time_entry { create(:time_entry, spent_on: start, hours: Chronos::DateTimeCalculations.in_hours(stop - start), user: user) }
   end
--- spec/factories/chronos_time_logs.rb
+++ spec/factories/chronos_time_logs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :time_log, class: 'Chronos::TimeLog' do
     user
-    start { Faker::Time.between Time.now, Time.now, :morning }
-    stop { Faker::Time.between Time.now, Time.now, :afternoon }
+    start { Faker::Time.between Date.today, Date.today, :morning }
+    stop { Faker::Time.between Date.today, Date.today, :afternoon }
     factory :time_log_with_comments do
       comments { Faker::Hacker.say_something_smart }
     end
