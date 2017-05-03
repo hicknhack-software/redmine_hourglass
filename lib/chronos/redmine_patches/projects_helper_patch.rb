f368838c42343551b3d6cb9e86f76fc64bade397
Robert Kranz <robert.kranz@hicknhack-software.com>
2017-05-03 16:46:34 +0200

    * small improvement to project settings tabs

--- lib/chronos/redmine_patches/projects_helper_patch.rb
+++ lib/chronos/redmine_patches/projects_helper_patch.rb
@@ -4,7 +4,9 @@ module Chronos
       extend ActiveSupport::Concern
 
       def project_settings_tabs
-        super + [{name: Chronos.plugin_name.to_s, action: :select_project_modules, partial: 'projects/chronos_settings', label: 'chronos.project_settings.title'}].select { |tab| @project.module_enabled?(Chronos.plugin_name) && User.current.allowed_to?(tab[:action], @project) }
+        super.tap do |tabs|
+          tabs << {name: Chronos.plugin_name.to_s, partial: 'projects/chronos_settings', label: 'chronos.project_settings.title'} if @project.module_enabled?(Chronos.plugin_name) && User.current.allowed_to?(:select_project_modules, @project)
+        end
       end
     end
   end
