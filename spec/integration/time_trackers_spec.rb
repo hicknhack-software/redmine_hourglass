223bb0c238d95e5dc6812cbea6096682bcd835ab
Robert Kranz <robert.kranz@hicknhack-software.com>
2017-06-14 13:09:43 +0200

    added show documentation

--- spec/integration/time_trackers_spec.rb
+++ spec/integration/time_trackers_spec.rb
@@ -10,7 +10,7 @@ describe 'Time trackers API', type: :request do
 
       include_examples 'access rights', :hourglass_view_tracked_time, :hourglass_view_own_tracked_time
 
-      response '200', 'returns currently active time trackers visible to the user' do
+      response '200', 'time trackers found' do
         schema type: 'array',
                items: {
                    '$ref': '#/definitions/time_tracker',
@@ -37,6 +37,43 @@ describe 'Time trackers API', type: :request do
           expect(data.second[:comments]).to eq @time_tracker2.comments
           expect(data.second[:project_id]).to eq @time_tracker2.project_id
           expect(data.second[:activity_id]).to eq @time_tracker2.activity_id
+        end
+      end
+    end
+  end
+
+  path '/time_trackers/{id}.json' do
+    get 'Returns the time tracker witht he given id' do
+      produces 'application/json'
+      tags 'Time trackers'
+      parameter name: :id, in: :path, type: :string
+
+      let(:user) { create :user, :as_member, permissions: [:hourglass_view_tracked_time] }
+      let(:time_tracker) do
+        User.current = user
+        Hourglass::TimeTracker.start comments: 'test'
+      end
+      let(:id) { time_tracker.id }
+
+      include_examples 'access rights', :hourglass_view_tracked_time, :hourglass_view_own_tracked_time
+
+      response '404', 'nothing found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+
+      response '200', 'time tracker found' do
+        schema '$ref': '#/definitions/time_tracker',
+               required: %w(id start user_id created_at updated_at)
+
+        include_examples 'has a valid response'
+
+        it 'returns correct data' do
+          data = JSON.parse(response.body, symbolize_names: true)
+          expect(data[:id]).to eq time_tracker.id
+          expect(Time.parse data[:start]).to eq time_tracker.start
+          expect(data[:user_id]).to eq time_tracker.user_id
+          expect(data[:comments]).to eq time_tracker.comments
         end
       end
     end
