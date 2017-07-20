768176f81e82b6ee14963da1dd0866c09d1ba37c
Robert Kranz <robert.kranz@hicknhack-software.com>
2017-07-20 14:47:54 +0200

    * fixed deleting booking time logs being allowed

--- app/policies/hourglass/time_log_policy.rb
+++ app/policies/hourglass/time_log_policy.rb
@@ -1,7 +1,7 @@
 module Hourglass
   class TimeLogPolicy < ApplicationPolicy
     def book?
-      @message = I18n.t('hourglass.api.time_logs.errors.already_booked') and return false if record.respond_to?(:booked?) && record.booked?
+      @message = I18n.t('hourglass.api.time_logs.errors.already_booked') and return false if booked?
       booking_allowed?
     end
 
@@ -9,8 +9,18 @@ module Hourglass
       authorized? :book
     end
 
+    def destroy?
+      @message = I18n.t('hourglass.api.time_logs.errors.delete_booked') and return false if booked?
+      super
+    end
+
     alias_method :bulk_book?, :book?
     alias_method :split?, :change?
     alias_method :join?, :change?
+
+    private
+    def booked?
+      record.respond_to?(:booked?) && record.booked?
+    end
   end
 end
