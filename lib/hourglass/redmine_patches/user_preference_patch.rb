dc087ec25005597af6ccfa0b067200070521ee01
Jonathan Sieber <jonathan.sieber@hicknhack-software.com>
2018-10-11 12:47:42 +0200

    Fix default_activity storage, with workaround for Redmine < 3.4

--- lib/hourglass/redmine_patches/user_preference_patch.rb
+++ lib/hourglass/redmine_patches/user_preference_patch.rb
@@ -1,7 +1,13 @@
 module Hourglass
   module RedminePatches
     module UserPreferencePatch
+
       extend ActiveSupport::Concern
+
+      included do
+        # Redmine >= 3.4 introduces "safe_attributes"
+        self.try :safe_attributes, 'default_activity'
+      end
 
       def default_activity
         self[:default_activity]
