32a2d274b5486729f9b289545f8761d2f61f00dd
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2019-01-27 00:36:25 +0100

    Reversed Merge pull request #90 from hicknhack-software/feature/consider_db_timezone
    Consider global ActiveRecord config of database timezone when constructing queries

--- app/models/hourglass/time_log_query.rb
+++ app/models/hourglass/time_log_query.rb
@@ -7,7 +7,7 @@ module Hourglass
     set_available_columns(
         comments: {},
         user: {sortable: lambda { User.fields_for_order_statement }, groupable: true},
-        date: {sortable: "#{queried_class.table_name}.start", groupable: sql_timezoned_date("#{queried_class.table_name}.start")},
+        date: {sortable: "#{queried_class.table_name}.start", groupable: "DATE(#{queried_class.table_name}.start)"},
         start: {},
         stop: {},
         hours: {totalable: true},
--- app/models/hourglass/time_tracker_query.rb
+++ app/models/hourglass/time_tracker_query.rb
@@ -5,7 +5,7 @@ module Hourglass
     set_available_columns(
         comments: {},
         user: {sortable: lambda { User.fields_for_order_statement }},
-        date: {sortable: "#{queried_class.table_name}.start", groupable: sql_timezoned_date("#{queried_class.table_name}.start")},
+        date: {sortable: "#{queried_class.table_name}.start", groupable: "DATE(#{queried_class.table_name}.start)"},
         start: {},
         hours: {},
         project: {sortable: "#{Project.table_name}.name", groupable: "#{Project.table_name}.id"},
