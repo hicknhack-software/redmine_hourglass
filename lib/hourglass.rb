19c4cb5634c2929b2c1752265a8a7671724054ce
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2019-02-26 14:26:03 +0100

    fixed version for release

--- lib/hourglass.rb
+++ lib/hourglass.rb
@@ -3,7 +3,7 @@ module Hourglass
   PLUGIN_NAME = "redmine_#{NAMESPACE}".to_sym
   PLUGIN_ROOT = Pathname.new(File.join File.dirname(__FILE__), '..').cleanpath
 
-  VERSION = '1.0-dev5'
+  VERSION = '1.0.0'
 
   def self.redmine_has_advanced_queries?
     Redmine::VERSION::MAJOR > 3 || (Redmine::VERSION::MAJOR == 3 && Redmine::VERSION::MINOR >= 4)
