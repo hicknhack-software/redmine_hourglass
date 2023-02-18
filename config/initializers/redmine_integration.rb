672bc52c6ecd44f534b2785f75bedc8d594179cb
pavanvo <anton@pavanvo.me>
2023-02-18 22:20:59 +0400

    fix: load patches

--- config/initializers/redmine_integration.rb
+++ config/initializers/redmine_integration.rb
@@ -7,12 +7,15 @@ if Rails.version < "5"
   ActionDispatch::Callbacks
 else
   ActiveSupport::Reloader
-end.to_prepare do
+end
+
+Rails.application.config.after_initialize do
   [Project, TimeEntry, User, UserPreference, TimeEntryActivity].each { |module_to_patch| add_patch module_to_patch }
   [ProjectsHelper, SettingsController].each { |module_to_patch| add_patch module_to_patch, method: (Rails.version < "5" ? :include : :prepend) }
   [Query].each { |module_to_patch| add_patch module_to_patch, method: :prepend }
 
   Redmine::Plugin.find(Hourglass::PLUGIN_NAME).extend Hourglass::RedminePatches::MirrorAssetsPatch
+  #Hourglass::Assets.compile if Rails.env.production?
 end
 
 Hourglass::RedmineHooks.load!
